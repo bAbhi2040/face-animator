42793fc567a014877aef7dbecf9cf47225f922fe
Abhirag B <bodiabhirag@gmail.com>
2026-07-13 19:01:31 -0500

    changes: - Used mediapipe framework to generate normalized coordinates for facial landmarks using their detection algorithim

--- test.py
+++ test.py
@@ -2,49 +2,65 @@ from PIL import Image
 import os
 import cv2
 import numpy as np
-
-i = 1
+import mediapipe as mp
 
 file_path = input("Copy and paste the image's file path: ").strip('"')
 if not os.path.exists(file_path):
     print('An error occured')
 else:
     src = cv2.imread(file_path)
-    output = src.copy()
 
     if src is None:
         print('OpenCV image opening error')
     else:
-        userChoice = input('Default, grayscale, blurred, or with Hough circles? ').lower()
+        appFunction = input('Select an option from the following: Image filtration, Facial animator...: ').lower()
 
-        gray = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-        def blur_generator(i, maxKernalLength, image):
-            while i < maxKernalLength:
-                i += 2
-                blurred_image = cv2.blur(image, ksize=(i, i))
-            return blurred_image
-        
-        if userChoice == 'default':
-            cv2.imshow('Default image', src)
-        elif userChoice == 'grayscale':
-            cv2.imshow('Grayscaled image', gray)
-        elif userChoice == 'blurred':
-            maxKernalLength = int(input('What is the max kernal length (must be odd)?: '))
-            blurred_image = blur_generator(i, maxKernalLength, src)
-            cv2.imshow('Blurred image', blurred_image)
-        elif userChoice == 'hough circles':
-            gray_and_blurred = blur_generator(i, 5, gray)
-            circles = cv2.HoughCircles(image=gray_and_blurred, method=cv2.HOUGH_GRADIENT, dp=1.2, minDist=30, param1=100, param2=30, minRadius=10, maxRadius=100)
-            if circles is not None:
-                circles = np.round(circles[0, :]).astype(int)
-                for x, y, r in circles:
-                    cv2.circle(gray_and_blurred, center=(x, y), radius=r, color=(0, 0, 0))
-                cv2.imshow('Hough circles', gray_and_blurred)
-            else:
-                print('error')
+        if appFunction == 'image filtration':
+            userChoice = input('Default, grayscale, blurred, or with Hough circles? ').lower()
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            gray = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+            def blur_generator(i, maxKernalLength, image):
+                while i < maxKernalLength:
+                    i += 2
+                    blurred_image = cv2.blur(image, ksize=(i, i))
+                return blurred_image
+            
+            if userChoice == 'default':
+                cv2.imshow('Default image', src)
+            elif userChoice == 'grayscale':
+                cv2.imshow('Grayscaled image', gray)
+            elif userChoice == 'blurred':
+                maxKernalLength = int(input('What is the max kernal length (must be odd)?: '))
+                blurred_image = blur_generator(1, maxKernalLength, src)
+                cv2.imshow('Blurred image', blurred_image)
+            elif userChoice == 'hough circles':
+                gray_and_blurred = blur_generator(1, 5, gray)
+                circles = cv2.HoughCircles(image=gray_and_blurred, method=cv2.HOUGH_GRADIENT, dp=1.2, minDist=30, param1=100, param2=30, minRadius=10, maxRadius=100)
+                if circles is not None:
+                    circles = np.round(circles[0, :]).astype(int)
+                    for x, y, r in circles:
+                        cv2.circle(gray_and_blurred, center=(x, y), radius=r, color=(0, 0, 0))
+                    cv2.imshow('Hough circles', gray_and_blurred)
+                else:
+                    print('error')
 
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
+        elif appFunction == 'facial animator':
+            model_path = r'C:\Users\abhid\Downloads\projs\testing\models\face_landmarker.task'
+            BaseOptions = mp.tasks.BaseOptions
+            FaceLandmarker = mp.tasks.vision.FaceLandmarker
+            FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+            VisionRunningMode = mp.tasks.vision.RunningMode
 
+            options = FaceLandmarkerOptions(
+                base_options=BaseOptions(model_asset_path=model_path),
+                running_mode=VisionRunningMode.IMAGE
+            )
+
+            with FaceLandmarker.create_from_options(options) as landmarker:
+                mp_image = mp.Image.create_from_file(file_path)
+                facemarker_results = landmarker.detect(mp_image)
+
+            print(facemarker_results)
