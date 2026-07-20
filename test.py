changes: - Encorporated MediaPipe's image segmentor algorithm to seperate a detected person from the background - Fixed image scaling issues to work with a native 1920x1080 resolution - Added both confidence and binary masks to accuratly detect the foreground from the background - Used morphology and Gaussian blur operations to smooth out masks

test.py
@@ -3,13 +3,14 @@ import os
 import cv2
 import numpy as np
 import mediapipe as mp
+from mediapipe.tasks.python import vision
+import matplotlib.pyplot as plt
 
 file_path = input("Copy and paste the image's file path: ").strip('"')
 if not os.path.exists(file_path):
     print('An error occured')
 else:
     src = cv2.imread(file_path)
-    print(src.shape)
 
     if src is None:
         print('OpenCV image opening error')
@@ -49,33 +50,62 @@ else:
             cv2.destroyAllWindows()
 
         elif appFunction == 'facial animator':
-            model_path = r'C:\Users\abhid\Downloads\projs\testing\models\face_landmarker.task'
+            model_path_face_landmark = r'C:\Users\abhid\Downloads\projs\testing\models\face_landmarker.task'
+            model_path_image_segmenter = r"C:\Users\abhid\Downloads\projs\testing\models\selfie_segmenter_landscape.tflite"
             BaseOptions = mp.tasks.BaseOptions
             FaceLandmarker = mp.tasks.vision.FaceLandmarker
             FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+            ImageSegmenter = mp.tasks.vision.ImageSegmenter
+            ImageSegmenterOptions = mp.tasks.vision.ImageSegmenterOptions
             VisionRunningMode = mp.tasks.vision.RunningMode
 
-            options = FaceLandmarkerOptions(
-                base_options=BaseOptions(model_asset_path=model_path),
+            optionsFaceLandmark = FaceLandmarkerOptions(
+                base_options=BaseOptions(model_asset_path=model_path_face_landmark),
                 running_mode=VisionRunningMode.IMAGE
             )
+            optionsImageSegmenter = ImageSegmenterOptions(
+                base_options=BaseOptions(model_asset_path=model_path_image_segmenter),
+                running_mode=VisionRunningMode.IMAGE,
+                output_category_mask=True,
+                output_confidence_masks=True
+            )
 
-            output = src.copy()
+            imageHeight, imageWidth, channels = src.shape
+            heightScale = 1080 / imageHeight
+            widthScale = 1920 / imageWidth
+            scale = min(heightScale, widthScale, 1)
+            scaledWidth = imageWidth * scale
+            scaledHeight = imageHeight * scale
+            output = cv2.resize(src, (int(scaledWidth), int(scaledHeight)))
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
 
-            with FaceLandmarker.create_from_options(options) as landmarker:
+            with FaceLandmarker.create_from_options(optionsFaceLandmark) as landmarker:
                 mp_image = mp.Image.create_from_file(file_path)
                 facemarker_results = landmarker.detect(mp_image)
 
-            imageHeight, imageWidth, channels = src.shape
             for landmark in facemarker_results.face_landmarks[0]:
-                x = int(landmark.x * imageWidth)
-                y = int(landmark.y * imageHeight)
+                x = int(landmark.x * scaledWidth)
+                y = int(landmark.y * scaledHeight)
                 cv2.circle(img=output, center=(x, y), radius=1, color=(0, 0, 255), thickness=-1)
-            
-            print(output.shape)
-            
-            cv2.namedWindow("Face Landmarks", cv2.WINDOW_NORMAL)
-            cv2.resizeWindow("Face Landmarks", (imageHeight, imageWidth))
-            cv2.imshow("Face Landmarks", output)
+
+            with ImageSegmenter.create_from_options(optionsImageSegmenter) as segmentor:
+                segmented_result = segmentor.segment(mp_image)
+                confidence_mask = segmented_result.confidence_masks[0]
+                alpha = (confidence_mask).numpy_view()
+                alpha = cv2.resize(alpha, (int(scaledWidth), int(scaledHeight)), interpolation=cv2.INTER_LINEAR)
+                alpha = cv2.GaussianBlur(alpha, (5, 5), 0)
+                
+                binary_mask = (alpha > 0.3).astype(np.uint8)
+                cv2.morphologyEx(binary_mask, cv2.MORPH_CLOSE, kernel, iterations=1, dst=binary_mask)
+                cv2.morphologyEx(binary_mask, cv2.MORPH_OPEN, kernel, iterations=1, dst=binary_mask)
+                alpha *= binary_mask
+
+                alpha = alpha[:, :, np.newaxis]
+                foreground = (output.astype(np.float32) * alpha).astype(np.uint8)
+                background = (output.astype(np.float32) * (1 - alpha)).astype(np.uint8)
+                
+            cv2.imshow("Mask", alpha)
+            cv2.imshow("Foreground", foreground)
+            cv2.imshow("Background", background)
             cv2.waitKey(0)
             cv2.destroyAllWindows()